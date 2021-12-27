3d4703c3d036f31da5cc900026b6838ef1e74ffb
SATRIO <satriotol69@gmail.com>
2021-12-28 00:19:44 +0700

    login done

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ResponseFormatter::success(
+            [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ],
+            'Berhasil Login'
+        );
+    }
+}
---
+++ database/seeders/UsersSeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = ['admin', 'user'];
+        foreach ($roles as $role => $value) {
+            Role::create([
+                'name' => $role,
+            ]);
+        };
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'role_id' => 1,
+            'phone' => 6289620755330
+        ]); 
+    }
+}
